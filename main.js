dd840fb794069a189f4585cb34e34798909baded
tigrius <mskrsin271@gmail.com>
2023-04-14 22:57:20 +0900

    color change

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry( 2, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 cube.position.x = 1;
