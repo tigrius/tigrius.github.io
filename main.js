3c2b388cc4977d2e2df42be2244877e96d5775e5
tigrius <130346548+tigrius@users.noreply.github.com>
2023-04-15 17:24:45 +0900

    main.js を更新

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const arjs = new THREEARL.LocationBased(scene , camera);
+const webcam = new THREEARL.WebcamRenderer(renderer);
+
 const geometry = new THREE.BoxGeometry( 2, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
 const cube = new THREE.Mesh( geometry, material );
