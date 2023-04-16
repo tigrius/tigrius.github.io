429ff4c0a6cb10d9c74e44e83f607ee8d8c6fc20
tigrius <mskrsin271@gmail.com>
2023-04-16 23:36:12 +0900

    revised

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //import * as THREE from 'three';
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-import * as THREEARL from 'https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 10000);
@@ -9,8 +8,8 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//const arjs = new THREEARL.LocationBased(scene , camera);
-//const webcam = new THREEARL.WebcamRenderer(renderer);
+//const arjs = new THREEx.LocationBased(scene , camera);
+//const webcam = new WebcamRenderer(renderer);
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
@@ -29,7 +28,7 @@ camera.position.z = 5;
 function animate(){
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.02;
+  cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
