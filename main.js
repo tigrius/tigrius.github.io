2c32babe91a1e55e8bd93613cfb1b583fd0205c0
tigrius <130346548+tigrius@users.noreply.github.com>
2023-04-16 17:04:04 +0900

    main.js を更新

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const geometry2 = new THREE.BoxGeometry(1,1,1);
 const material2 = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
 const box = new THREE.Mesh( geometry2, material2);
 scene.add( box);
-box.position.x = 3;
+box.position.x = -1;
 
 camera.position.z = 5;
 
