5cd76c36604b50187b0d392aab9696120e0efa8a
tigrius <mskrsin271@gmail.com>
2023-07-15 18:17:07 +0900

    reviced function

--- functions.js
+++ functions.js
@@ -14,17 +14,39 @@ function nextAreaC(trgl){
                 return [trgl[0],-trgl[1]];
             }
             else{
-                return [(trgl[0] - Math.round((trgl[0]-1)/4))%15, 3*trgl[1] /4];
+                if (trgl[1] >= 0){
+                    return [(trgl[0] - Math.round((trgl[0]-1)/4))%15, 3*trgl[1] /4];
+                }
+                else{
+                    return [(trgl[0] - Math.floor(trgl[0]/4))%15, 3*trgl[1] /4];
+                }
             }
         case 3:
-            if (trgl[0] % 3 == 0){
-                return [trgl[0] /3, trgl[1] /3];
+            if (trgl[1] >= 0){
+                if (trgl[0] % 3 == 0){
+                    return [trgl[0] /3, trgl[1] /3];
+                }
+                else{
+                    return [(trgl[0] + Math.round(trgl[0]/3))%20, 4* trgl[1]/3];
+                }
             }
             else{
-                return [(trgl[0] + Math.round(trgl[0]/3))%20, 4* trgl[1]/3];
+                if ((trgl[0]-1) % 3 == 0){
+                    return [(trgl[0]-1) /3, trgl[1] /3];
+                }
+                else{
+                    return [(trgl[0] + Math.floor(trgl[0]/3))%20, 4* trgl[1]/3];
+                }
             }
+            
         case 1:
-            return [trgl[0] *3, trgl[1] *3];
+            if (trgl[1] >= 0){
+                return [trgl[0] *3, trgl[1] *3];
+            }
+            else{
+                return [trgl[0] *3 +1, trgl[1] *3];
+            }
+            
     }
 }
 
